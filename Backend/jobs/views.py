71aaab85e9357b3dece986294afa9274bd4cd029
Alistair Francis <alistairfrancis.7@gmail.com>
2025-03-09 15:14:07 +0000

    landing page changed

--- Backend/jobs/views.py
+++ Backend/jobs/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+import json
 
-# Create your views here.
+
+#TODO: 1. take i/p from html form - job (description) 
+#TODO: 2. resume/cv i/p 
+#TODO: 3. convert both into json n send to groq 
+#TODO: 4. recieve the percentage of probability of getting interview call 
+#TODO: 5. send data to frontend html n show result to user 
+class job_compare_2_cv(APIView):
+    def extract(request):
+        if request.POST:
+            variable1=1
+
+    pass
